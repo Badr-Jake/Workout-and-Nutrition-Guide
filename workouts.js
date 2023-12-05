bb3a0d1616b0e7901ca3073686a5a24426045e8a
unknown <world2222222222@gmail.com>
2023-12-05 12:35:56 +0900

    FEAT: Change the style and make a server
    Changed the look of the pages and also made a server but it's not yet on
    service well.

--- workouts.js
+++ workouts.js
@@ -14,5 +14,23 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Show the selected explanation
     explanations[index].style.display = 'block';
+
+    // Get the current day
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const currentDay = days[new Date().getDay()];
+
+    // Find and highlight lines corresponding to the current day
+    const lines = document.querySelectorAll(`.explanation-list li`);
+    lines.forEach((line) => {
+      line.style.border = '2px solid transparent'; // Set initial border style
+
+      // Check if the line contains the current day
+      if (line.textContent.includes(`${currentDay}:`)) {
+        // Apply dynamic border color
+        line.style.borderImage = 'linear-gradient(90deg, violet, indigo, blue, green, yellow, orange, red)';
+        line.style.borderImageSlice = 1;
+        line.style.animation = 'rainbow-border 6s linear infinite';
+      }
+    });
   }
 });
